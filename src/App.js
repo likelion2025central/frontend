082475f11206b09da6204f71e5edef163f7ab501
jinsujini <csujin2040@gmail.com>
2025-08-20 20:08:37 +0900

    fix : 빈 폴더 제거

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Splash from './components/Section/Splash'
 import Nav from './components/Section/Nav'
 import Main from './components/Section/Main'
-import Registration from './components/Registration/Registration'
+
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Splash />} />
         <Route path='/main' element={<Main />} />
-        <Route path='/register' element={<Registration />} />
+
 
 
       </Routes>
